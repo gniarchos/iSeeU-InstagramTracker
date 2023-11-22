96ea1c5357c6ce58910c6955dda5d176a3076df0
gniarchos <giannisnrch@gmail.com>
2023-11-22 21:06:28 +0200

    updated help information

--- src/components/Help.js
+++ src/components/Help.js
@@ -3,15 +3,82 @@ import React from "react"
 export default function Help() {
   return (
     <div className="help-div">
-      <h1>Help: How to download your files</h1>
+      <h1>Guide: How to download your files</h1>
       <p className="help-source">
         Source:
         <a href="https://help.instagram.com/181231772500920">
           Instagram Help Center
-        </a>
+        </a>{" "}
+        - Updated: 22/11/2023
       </p>
 
       <div>
+        <div className="help-methods">
+          <div className="help-title-div">
+            <h2>
+              How to download your data from <u>iOS</u> and <u>Android</u> app
+            </h2>
+          </div>
+
+          <ol>
+            <li className="li-computer">
+              Download Instagram app and <b>Login</b> to your account.
+            </li>
+            <li className="li-mobile">
+              Tap your profile picture in the bottom right to go to your
+              profile.
+            </li>
+            <li className="li-mobile">
+              Tap on <b>Settings</b> icon in the top right and then{" "}
+              <b>Your Activity</b>.
+            </li>
+            <li className="li-mobile">
+              Tap <b>Download your information.</b>
+            </li>
+            <li className="li-mobile">
+              Tap <b>Request a download</b>.
+            </li>
+            <li className="li-mobile">
+              Select your instagram account you want to download your data and
+              tap <b>Next</b>.
+            </li>
+            <li className="li-mobile">
+              Tap <b>Select types of information</b> and select only{" "}
+              <b>Followers and following</b>.
+            </li>
+            <li className="li-mobile">
+              <u>
+                <b>IMPORTANT!</b>
+              </u>
+              &nbsp;Tap <b>Date range</b> and select <b>All time</b>, then tap{" "}
+              <b>Save</b>.
+            </li>
+            <li className="li-mobile">
+              <u>
+                <b>IMPORTANT!</b>
+              </u>
+              &nbsp;Tap <b>Format</b> and select <b>JSON</b>, then tap{" "}
+              <b>Save.</b>
+            </li>
+            <li className="li-mobile">
+              Tap <b>Submit Request.</b>
+            </li>
+            <li className="li-mobile">
+              You'll soon receive an email titled{" "}
+              <b>"Your Meta information download is ready"</b> and follow
+              instructions to finish downloading your information.
+            </li>
+            <li className="li-mobile">
+              Save the file somewhere in your filesystem of your phone and{" "}
+              <b>Extract it.</b>
+            </li>
+            <li className="li-mobile">
+              Inside the extracted folder, you will find the required files:{" "}
+              <b>following.json</b> and <b>followers_1.json.</b> These are the
+              files you need to upload.
+            </li>
+          </ol>
+        </div>
         <div className="help-methods">
           <div className="help-title-div">
             <h2>
@@ -21,140 +88,133 @@ export default function Help() {
 
           <ol>
             <li className="li-computer">
-              Go to your preferred web browser and navigate to{" "}
-              <b> instagram.com</b>.
-            </li>
-            <li className="li-computer">
-              <b>Login</b> to your account.
+              Go to your preferred web browser, navigate to{" "}
+              <b> instagram.com</b> and <b>Login</b> to your account.
             </li>
             <li className="li-mobile">
               Tap your profile picture in the bottom right to go to your
               profile.
             </li>
             <li className="li-mobile">
-              Tap <b>Your Activity</b>.
+              Tap on <b>Settings</b> icon in the top left and then{" "}
+              <b>Your Activity</b>.
             </li>
             <li className="li-mobile">
               Tap <b>Download your information.</b>
             </li>
             <li className="li-mobile">
-              Enter the email address where you'd like to receive a link to your
-              data.
+              Tap <b>Continue</b> to navigate to Accounts Center.
+            </li>
+            <li className="li-mobile">
+              Tap <b>Request a download</b>.
+            </li>
+            <li className="li-mobile">
+              Select your instagram account you want to download your data and
+              tap <b>Next</b>.
+            </li>
+            <li className="li-mobile">
+              Tap <b>Select types of information</b> and select only{" "}
+              <b>Followers and following</b>.
             </li>
             <li className="li-mobile">
               <u>
                 <b>IMPORTANT!</b>
               </u>
-              &nbsp;Tap the checkbox next to <b>JSON</b> to select the correct
-              format that you must receive your data in, then tap
-              <b> Next.</b>
+              &nbsp;Tap <b>Date range</b> and select <b>All time</b>, then tap{" "}
+              <b>Save</b>.
             </li>
             <li className="li-mobile">
-              Enter your Instagram account password and tap
-              <b> Request download.</b>
+              <u>
+                <b>IMPORTANT!</b>
+              </u>
+              &nbsp;Tap <b>Format</b> and select <b>JSON</b>, then tap{" "}
+              <b>Save.</b>
             </li>
             <li className="li-mobile">
-              You'll soon receive an email titled <b>Your Instagram data </b>
-              with a link to your data. Tap <b>Download data</b> and follow the
+              Tap <b>Submit Request.</b>
+            </li>
+            <li className="li-mobile">
+              You'll soon receive an email titled{" "}
+              <b>"Your Meta information download is ready"</b> and follow
               instructions to finish downloading your information.
             </li>
             <li className="li-mobile">
-              Save the file somewhere in your filesystem of your phone and
-              <b> Extract it.</b>
+              Save the file somewhere in your filesystem of your phone and{" "}
+              <b>Extract it.</b>
             </li>
             <li className="li-mobile">
-              Inside the extracted folder you should navigate to the folder with
-              the name <b>followers_and_following.</b>
-            </li>
-            <li className="li-mobile">
-              The required files you will need to upload are
-              <b> following.json</b> and <b>followers_1.json.</b>
+              Inside the extracted folder, you will find the required files:{" "}
+              <b>following.json</b> and <b>followers_1.json.</b> These are the
+              files you need to upload.
             </li>
           </ol>
+        </div>
+      </div>
 
-          <p>
-            <b>Note:</b> Instagram states that it may take up to 30 days for
-            them to email you a download link, but usually you will receive the
-            email sooner (from minutes to hours).
-          </p>
+      <div className="help-methods">
+        <div className="help-title-div">
+          <h2>
+            How to download your data from <u>computer</u> website
+          </h2>
         </div>
 
-        <div className="help-methods">
-          <div className="help-title-div">
-            <h2>
-              How to download your data from <u>computer</u> website
-            </h2>
-          </div>
-
-          <ol>
-            <li className="li-computer">
-              Go to your preferred web browser and navigate to{" "}
-              <b> instagram.com</b>.
-            </li>
-            <li className="li-computer">
-              <b>Login</b> to your account.
-            </li>
-            <li className="li-computer">
-              Click <b>More</b> in the bottom left, then click{" "}
-              <b>Your Activity</b>.
-            </li>
-            <li className="li-computer">
-              Click <b>Download your information</b>
-            </li>
-            <li className="li-computer">
-              Enter the email address where you'd like to receive a link to your
-              data.
-            </li>
-            <li className="li-computer">
-              <u>
-                <b>IMPORTANT!</b>
-              </u>
-              &nbsp;Click the checkbox next to <b>JSON</b> to select the correct
-              format that you must receive your data in, then tap
-              <b> Next.</b>
-            </li>
-            <li className="li-computer">
-              Enter your Instagram account password and click
-              <b> Request download.</b>
-            </li>
-            <li className="li-computer">
-              You'll soon receive an email titled <b>Your Instagram data </b>
-              with a link to your data. Click <b>Download data</b> and follow
-              the instructions to finish downloading your information.
-            </li>
-            <li className="li-computer">
-              Save the file somewhere in your filesystem of your computer and
-              <b> Extract it.</b>
-            </li>
-            <li className="li-computer">
-              Inside the extracted folder you should navigate to the folder with
-              the name <b>followers_and_following.</b>
-            </li>
-            <li className="li-computer">
-              The required files you will need to upload are
-              <b> following.json</b> and <b>followers_1.json.</b>
-            </li>
-          </ol>
-
-          <p>
-            <b>Note:</b> Instagram states that it may take up to 30 days for
-            them to email you a download link, but usually you will receive the
-            email sooner (from minutes to hours).
-          </p>
-        </div>
-
-        <div className="help-methods">
-          <div className="help-title-div">
-            <h2>How to download your data from iOS and Android app</h2>
-          </div>
-
-          <div className="help-app-answer">
-            <p>
-              This method only works with browser version of Instagram. For more
-              information, please refer to the section above.
-            </p>
-          </div>
-        </div>
+        <ol>
+          <li className="li-computer">
+            Go to your preferred web browser, navigate to <b> instagram.com</b>{" "}
+            and <b>Login</b> to your account.
+          </li>
+          <li className="li-computer">
+            Click <b>More</b> in the bottom left and then <b>Your Activity</b>.
+          </li>
+          <li className="li-computer">
+            Click <b>Download your information</b>
+          </li>
+          <li className="li-mobile">
+            Click <b>Continue</b> to navigate to Accounts Center.
+          </li>
+          <li className="li-mobile">
+            Click <b>Request a download</b>.
+          </li>
+          <li className="li-mobile">
+            Select your instagram account you want to download your data and
+            click <b>Next</b>.
+          </li>
+          <li className="li-mobile">
+            Click <b>Select types of information</b> and select only{" "}
+            <b>Followers and following</b>.
+          </li>
+          <li className="li-mobile">
+            <u>
+              <b>IMPORTANT!</b>
+            </u>
+            &nbsp;Click <b>Date range</b> and select <b>All time</b> and click{" "}
+            <b>Save</b>.
+          </li>
+          <li className="li-mobile">
+            <u>
+              <b>IMPORTANT!</b>
+            </u>
+            &nbsp;Click <b>Format</b> and select <b>JSON</b>, then click{" "}
+            <b>Save.</b>
+          </li>
+          <li className="li-mobile">
+            Click <b>Submit Request.</b>
+          </li>
+          <li className="li-mobile">
+            You'll soon receive an email titled{" "}
+            <b>"Your Meta information download is ready"</b> and follow
+            instructions to finish downloading your information.
+          </li>
+          <li className="li-computer">
+            Save the file somewhere in your filesystem of your computer and{" "}
+            <b>Extract it.</b>
+          </li>
+          <li className="li-mobile">
+            Inside the extracted folder, you will find the required files:{" "}
+            <b>following.json</b> and <b>followers_1.json.</b> These are the
+            files you need to upload.
+          </li>
+        </ol>
       </div>
     </div>
   )
