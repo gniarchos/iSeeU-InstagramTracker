793aa62e1117ea9048b159ba8f1c0bca0c67bae3
Giannis Niarchos <giannisnrch@gmail.com>
2024-08-22 20:30:10 +0300

    Updates

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+// public/service-worker.js
+
+const CACHE_NAME = "my-app-cache-v1"
+const urlsToCache = ["/", "/index.html" /* other assets */]
+
+// Install the service worker
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(urlsToCache)
+    })
+  )
+})
+
+// Intercept fetch requests
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request)
+    })
+  )
+})
+
+// Activate the service worker
+self.addEventListener("activate", (event) => {
+  const cacheWhitelist = [CACHE_NAME]
+  event.waitUntil(
+    caches.keys().then((cacheNames) => {
+      return Promise.all(
+        cacheNames.map((cacheName) => {
+          if (!cacheWhitelist.includes(cacheName)) {
+            return caches.delete(cacheName)
+          }
+        })
+      )
+    })
+  )
+})
