9abe4245b9e5552f95dfccb721835b38562120df
Giannis Niarchos <giannisnrch@gmail.com>
2026-08-30 20:51:11 +0300

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
-import { defineConfig } from "vite"
+import { defineConfig, loadEnv } from "vite"
 import react from "@vitejs/plugin-react"
+import pkg from "./package.json"
 
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    port: 3000,
-    host: true,
-  },
-  base: "/iSeeU-InstagramTracker/", // <-- set this if deploying on GitHub Pages
+export default defineConfig(({ mode }) => {
+  // Load all env variables from .env, .env.local, .env.[mode], and importantly .env.version
+  const env = loadEnv(mode, process.cwd())
+
+  return {
+    plugins: [react()],
+    server: {
+      port: 3000,
+      host: true,
+      open: true,
+    },
+    define: {
+      // Make VITE_APP_VERSION available in your client-side code as import.meta.env.VITE_APP_VERSION
+      "process.env": env,
+      __APP_VERSION__: JSON.stringify(pkg.version),
+    },
+    base: "/iSeeU-InstagramTracker/", // <-- set this if deploying on GitHub Pages
+  }
 })
